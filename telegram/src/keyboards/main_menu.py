6a405dc0918f2dea35429d8992979ab713a0def9
Владимир Волосников <volosnikovvladimir68@gmail.com>
2025-05-25 00:04:13 +0500

    Minor fixes

--- telegram/src/keyboards/main_menu.py
+++ telegram/src/keyboards/main_menu.py
@@ -1,4 +1,5 @@
 """The module responsible for assembling the keyboard with the main menu."""
+
 from logging import getLogger
 
 from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
@@ -15,15 +16,16 @@ def build_kb_with_main_menu() -> InlineKeyboardMarkup:
     kb_builder: InlineKeyboardBuilder = InlineKeyboardBuilder()
 
     catalog_bt: InlineKeyboardButton = InlineKeyboardButton(
-        text=LEXICON_RU["catalog_bt"]
+        text=LEXICON_RU["catalog_bt"],
+        callback_data="catalog_bt",
     )
     shopping_cart_bt: InlineKeyboardButton = InlineKeyboardButton(
-        text=LEXICON_RU["shopping_cart_bt"]
+        text=LEXICON_RU["shopping_cart_bt"],
+        callback_data="shopping_cart_bt",
     )
     faq_bt: InlineKeyboardButton = InlineKeyboardButton(
-        text=LEXICON_RU["faq_bt"]
+        text=LEXICON_RU["faq_bt"], callback_data="faq_bt"
     )
 
     kb_builder.row(*(catalog_bt, shopping_cart_bt, faq_bt), width=3)
-
     return kb_builder.as_markup(resize_keyboard=True)
