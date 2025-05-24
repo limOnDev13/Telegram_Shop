e3f8d3b169e4a5f4e1a6526a0124a6051318229f
Владимир Волосников <volosnikovvladimir68@gmail.com>
2025-05-24 23:59:21 +0500

    Create func get_channels_to_subscribe; fix func check_subscription_on_channels - now subscription channels are taken from Redis, and if they are not there, then from Postgres; remove func get_channel_title_and_url;

--- telegram/src/services/subscription_verification.py
+++ telegram/src/services/subscription_verification.py
@@ -1,15 +1,23 @@
 """The module responsible for verifying user subscriptions."""
+
 from logging import getLogger
-from typing import List, Tuple
+from typing import List, Optional, Set
 
 from aiogram import Bot
-from aiogram.exceptions import TelegramNotFound
+from aiogram.exceptions import TelegramBadRequest
 from aiogram.types import (
-    ChatFullInfo,
     ChatMember,
     ChatMemberAdministrator,
     ChatMemberMember,
     ChatMemberOwner,
+)
+
+from repositories.channels_to_subscribe.redis import (
+    RedisChannelsToSubscribeRepository,
+)
+from schemas.channels_to_subscribe import ChannelToSubscribeSchema
+from telegram.src.db.repositories.channels_to_subscribe import (
+    SQLAlchemyChannelsToSubscribeRepository,
 )
 
 logger = getLogger("telegram.services.subscription_verification")
@@ -28,42 +36,58 @@ async def subscription_verification(
     )
 
 
+async def get_channels_to_subscribe(
+    redis_channels_repo: RedisChannelsToSubscribeRepository,
+    alchemy_channels_repo: SQLAlchemyChannelsToSubscribeRepository,
+) -> Set[ChannelToSubscribeSchema]:
+    """
+    Get channels to subscribe.
+
+    The function first searches for data in redis,
+    if it doesn't find it, it searches in postgres (using sqlalchemy).
+    If it doesn't find it, it returns an empty set.
+    :param redis_channels_repo: redis channels repository.
+    :param alchemy_channels_repo: sqlalchemy channels repository.
+    :return: Set of channels to subscribe.
+    """
+    channels: Optional[Set[ChannelToSubscribeSchema]] = (
+        await redis_channels_repo.get_all()
+    )
+    if channels is None:
+        channels = await alchemy_channels_repo.get_all()
+        if channels is None:
+            return set()
+    return channels
+
+
 async def check_subscription_on_channels(
-    user_id: int, bot: Bot, channels: List[str]
-) -> List[str]:
+    user_id: int,
+    bot: Bot,
+    redis_channels_repo: RedisChannelsToSubscribeRepository,
+    alchemy_channels_repo: SQLAlchemyChannelsToSubscribeRepository,
+) -> List[ChannelToSubscribeSchema]:
     """
     Check the user's subscriptions for each channel.
 
     The function returns a list of channel IDs that the user is not subscribed to.
     :param user_id: User ID.
     :param bot: Bot object.
-    :param channels: List of channel IDs.
-    :return: A list of channel IDs that the user is not subscribed to.
+    :param redis_channels_repo: channels repository based on redis.
+    :param alchemy_channels_repo: channels repository based on sqlalchemy[postgres]
+    :return: A List of ChannelToSubscribeSchema that the user is not subscribed to.
     If the list is empty, then the user is subscribed to all channels.
     """
+    channels: Set[ChannelToSubscribeSchema] = await get_channels_to_subscribe(
+        redis_channels_repo, alchemy_channels_repo
+    )
     logger.debug("Check user subscription on channels: %s", str(channels))
-    without_subscription: List[str] = list()
+    without_subscription: List[ChannelToSubscribeSchema] = list()
 
-    for channel_id in channels:
+    for channel in channels:
         try:
-            if await subscription_verification(user_id, bot, channel_id):
-                without_subscription.append(channel_id)
-        except TelegramNotFound as exc:
-            logger.error("User or chat not found. %s", str(exc))
+            if not await subscription_verification(user_id, bot, channel.id):
+                without_subscription.append(channel)
+        except TelegramBadRequest as exc:
+            logger.error(str(exc))
 
     return without_subscription
-
-
-async def get_channel_title_and_url(
-    channel_id: str, bot: Bot
-) -> Tuple[str, str]:
-    """Return channel title and url."""
-    channel: ChatFullInfo = await bot.get_chat(channel_id)
-    title: str = channel.title
-    if channel.username:
-        url: str = f"https://t.me/{channel.username}"
-    else:
-        url = f"https://t.me/{channel_id}"
-
-    logger.debug("Chat info: id: %s; title: %s; username: %s; url: %s")
-    return title, url
