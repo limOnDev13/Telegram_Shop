1a326c5c8b5d80a8c29b3643cff3ed1e4e379fa0
Владимир Волосников <volosnikovvladimir68@gmail.com>
2025-05-24 23:57:11 +0500

    Minor fix

--- telegram/src/lexicon/ru.py
+++ telegram/src/lexicon/ru.py
@@ -1,11 +1,12 @@
 """The module responsible for the Russian lexicon."""
+
 from typing import Dict
 
 LEXICON_RU: Dict[str, str] = {
     "start": "Приветствую!",
     "subscription_verification": "Проверяю подписку на каналы...",
     "failed_subscription_verification": "Вы не подписаны на эти каналы. "
-                                        "Подпишитесь и снова вызовете команду /start.",
+    "Подпишитесь и снова вызовете команду /start.",
     "successful_subscription_verification": "Главное меню",
     "catalog_bt": "Каталог",
     "shopping_cart_bt": "Корзина",
