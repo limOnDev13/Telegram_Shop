45d265012765d3d11a2d443502baca912755680d
Владимир Волосников <volosnikovvladimir68@gmail.com>
2025-05-25 19:33:22 +0500

    Add handler pagination_categories

--- telegram/src/handlers/catalog.py
+++ telegram/src/handlers/catalog.py
@@ -1,7 +1,7 @@
 """The module responsible for the handles, responsible for the catalog."""
 
 from logging import getLogger
-from typing import Any, Dict
+from typing import Optional
 
 from aiogram import F, Router
 from aiogram.filters import StateFilter
@@ -11,10 +11,9 @@ from aiogram.types import CallbackQuery
 from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker
 
 from telegram.src.config.app import Config
-from telegram.src.db.repositories.categories import AlchemyCategoryRepository
+from telegram.src.db.queries.categories import get_number_children
 from telegram.src.keyboards import BACK_TO_MAIN_MENU_CALLBACK, CATEGORY_CB
 from telegram.src.keyboards.categories import build_kb_with_categories
-from telegram.src.lexicon.ru import LEXICON_RU
 from telegram.src.states.catalog import FSMCatalog
 
 logger = getLogger("telegram.handlers.catalog")
@@ -23,11 +22,6 @@ router: Router = Router()
 
 @router.callback_query(F.data == BACK_TO_MAIN_MENU_CALLBACK)
 @router.callback_query(StateFilter(default_state), F.data == CATEGORY_CB)
-# cb format - category:page={page}
-@router.callback_query(
-    StateFilter(FSMCatalog.choose_category),
-    F.data.startswith("category:page="),
-)
 async def show_categories(
     cb: CallbackQuery,
     state: FSMContext,
@@ -36,28 +30,65 @@ async def show_categories(
 ) -> None:
     """Show categories."""
     logger.debug("Show categories")
-    categories_repo = AlchemyCategoryRepository(Session)
 
-    if cb.data == CATEGORY_CB or cb.data == BACK_TO_MAIN_MENU_CALLBACK:
-        page = 0
-        per_page = config.categories_per_page
-        count_categories = await categories_repo.length()
-        logger.debug("Number categories: %d", count_categories)
-        pages = count_categories // per_page
-        await state.set_state(FSMCatalog.choose_category)
-        await state.update_data(pages=pages, per_page=per_page)
-    else:
-        page = int(cb.data.split(":")[1].split("=")[1])
-        pagination_data: Dict[str, Any] = await state.get_data()
-        per_page, pages = pagination_data["per_page"], pagination_data["pages"]
+    category_id: Optional[int] = None
+    path: str = "/"
+    page = 0
+    per_page = config.categories_per_page
+    count_categories = await get_number_children(Session)
+    logger.debug("Number categories: %d", count_categories)
+    pages = count_categories // per_page
+    await state.set_state(FSMCatalog.choose_category)
 
     logger.debug("Number pages: %d", pages)
     logger.debug("Current page: %d", page)
     logger.debug("Per page: %d", per_page)
+    logger.debug("Path: %s", path)
+    logger.debug("Root: %s", str(category_id))
 
     await cb.message.edit_text(
-        text=LEXICON_RU["catalog"],
+        text=path,
         reply_markup=await build_kb_with_categories(
-            categories_repo, page, pages, per_page
+            Session, page, pages, path, per_page, category_id
+        ),
+    )
+
+
+@router.callback_query(
+    StateFilter(FSMCatalog.choose_category),
+    F.data.startswith("category:page="),
+)
+async def pagination_categories(
+    cb: CallbackQuery,
+    config: Config,
+    Session: async_sessionmaker[AsyncSession],
+) -> None:
+    """Paginate categories."""
+    logger.debug("Show categories")
+
+    data_str: str = cb.data.split(":")[1]
+    page_str, category_id_str, path_str = data_str.split(";")
+    page = int(page_str.split("=")[1])
+    category_id_str = category_id_str.split("=")[1]
+    category_id: Optional[int] = (
+        None if category_id_str == "" else int(category_id_str)
+    )
+    path = path_str.split("=")[1]
+
+    per_page = config.categories_per_page
+    count_categories = await get_number_children(Session, root_id=category_id)
+    logger.debug("Number categories: %d", count_categories)
+    pages = count_categories // per_page
+
+    logger.debug("Number pages: %d", pages)
+    logger.debug("Current page: %d", page)
+    logger.debug("Per page: %d", per_page)
+    logger.debug("Path: %s", path)
+    logger.debug("Root: %s", str(category_id))
+
+    await cb.message.edit_text(
+        text=path,
+        reply_markup=await build_kb_with_categories(
+            Session, page, pages, path, per_page, category_id
         ),
     )
