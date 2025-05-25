a1a435d118af8f698928fc883812f898c0560ca6
Владимир Волосников <volosnikovvladimir68@gmail.com>
2025-05-25 19:27:00 +0500

    Add parent when generate new category

--- telegram/src/db/factories/categories.py
+++ telegram/src/db/factories/categories.py
@@ -1,10 +1,13 @@
 """Module with factories."""
 
+import random
+from typing import List
+
 import factory
 
 from telegram.src.db.models import Category
 
-from .base import BaseFactory
+from .base import BaseFactory, Session
 
 
 class CategoryFactory(BaseFactory):
@@ -20,5 +23,29 @@ class CategoryFactory(BaseFactory):
 
 if __name__ == "__main__":
     count_categories: int = int(input("Number categories: "))
+    one_tree: str = input("One tree?(yes/no) ").strip().lower()
 
-    CategoryFactory.create_batch(size=count_categories)
+    with Session() as session:
+        categories: List[Category] = list()
+        for i in range(count_categories):
+            if i == 0:
+                category = CategoryFactory.build(parent=None, parent_id=None)
+            else:
+                random_parent = random.choice(categories)
+                if one_tree == "yes":
+                    category = CategoryFactory.build(
+                        parent=random_parent, parent_id=random_parent.id
+                    )
+                else:
+                    if random.random() < 0.5:
+                        category = CategoryFactory.build(
+                            parent=random_parent, parent_id=random_parent.id
+                        )
+                    else:
+                        category = CategoryFactory.build(
+                            parent=None, parent_id=None
+                        )
+            session.add(category)
+            session.commit()
+            session.refresh(category)
+            categories.append(category)
