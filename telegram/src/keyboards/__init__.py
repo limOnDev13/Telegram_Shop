9b3d0a80dceb4ede8d43d0ed9692e30dd05c3949
Владимир Волосников <volosnikovvladimir68@gmail.com>
2025-05-25 19:31:29 +0500

    Change cb data format

--- telegram/src/keyboards/__init__.py
+++ telegram/src/keyboards/__init__.py
@@ -1,6 +1,8 @@
 """The package responsible for the keyboard of the bot."""
 
 CATEGORY_CB: str = "category"
-CATEGORY_PAGINATION: str = "category:page={page}"
+CATEGORY_PAGINATION: str = (
+    "category:page={page};category_id={category_id};path={path}"
+)
 CATEGORY_CUR_PAGE_CALLBACK: str = "category:cur_page"
 BACK_TO_MAIN_MENU_CALLBACK: str = "back_to_main_menu"
