568d3fb731568a72ebc9de645ecf60d4277f5594
Владимир Волосников <volosnikovvladimir68@gmail.com>
2025-05-25 12:57:46 +0500

    Create table category

---
+++ telegram/src/db/models/category.py
@@ -0,0 +1,14 @@
+"""The module responsible for the category model."""
+
+from sqlalchemy import Integer, String
+from sqlalchemy.orm import Mapped, mapped_column
+
+from .base import Base
+
+
+class Category(Base):
+
+    __tablename__ = "category"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+    name: Mapped[str] = mapped_column(String(20), nullable=False, unique=True)
