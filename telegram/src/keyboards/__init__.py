5aff638c4daf796377a6b1ff1e0d1bddc06dc169
Владимир Волосников <volosnikovvladimir68@gmail.com>
2025-05-25 13:09:30 +0500

    Add vars

--- telegram/src/keyboards/__init__.py
+++ telegram/src/keyboards/__init__.py
@@ -1,4 +1,5 @@
 """The package responsible for the keyboard of the bot."""
 
-from .channels_to_subscribe import build_kb_with_channels_to_subscribe
-from .main_menu import build_kb_with_main_menu
+CATEGORY_PAGINATION_CALLBACK: str = "category:page-{page};per_page-{per_page};pages-{pages}"
+CATEGORY_CUR_PAGE_CALLBACK: str = "category:cur_page"
+BACK_TO_MAIN_MENU_CALLBACK: str = "back_to_main_menu"
