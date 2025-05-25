1ba6c890ab5df63012df2ecb195851253d72dfb5
Владимир Волосников <volosnikovvladimir68@gmail.com>
2025-05-25 12:55:53 +0500

    Create category schema

---
+++ schemas/categories.py
@@ -0,0 +1,13 @@
+from pydantic import BaseModel, Field
+
+
+class CategorySchema(BaseModel):
+    """Category schema."""
+
+    id: int = Field(..., description="Category ID.")
+    name: str = Field(..., description="Category name.")
+
+    class Config:
+        """Config class."""
+
+        from_attributes = True
