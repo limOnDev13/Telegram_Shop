1057bc1c223087d89b490a38781c2c6bfc40f404
Владимир Волосников <volosnikovvladimir68@gmail.com>
2025-05-27 15:02:29 +0500

    Add new global cb vars

--- telegram/src/keyboards/__init__.py
+++ telegram/src/keyboards/__init__.py
@@ -7,4 +7,16 @@ CATEGORY_PAGINATION: str = (
 CATEGORY_CUR_PAGE_CALLBACK: str = "category:cur_page"
 BACK_TO_MAIN_MENU_CALLBACK: str = "back_to_main_menu"
 PRODUCT_CB: str = "product:category_id={category_id}"
-BUY_PRODUCT_CB: str = "shopping_cart:add:product_id={product_id}"
+BUY_PRODUCT_CB: str = "product|buy:product_id={product_id}"
+SHOPPING_CART_CB: str = "shopping_cart"
+SHOPPING_CART_BUY_PRODUCT_CB: str = (
+    "shopping_cart|view_product:product_id={product_id};count={count}"
+)
+SHOPPING_CART_PAGINATION_CB: str = "shopping_cart|pagination:page={page}"
+SHOPPING_CART_CUR_PAGE_CB: str = "shopping_cart|current_page"
+SHOPPING_CART_REMOVE_PRODUCT_CB: str = (
+    "shopping_cart|remove:product_id={product_id}"
+)
+SHOPPING_CART_MAKING_ORDER: str = (
+    "shopping_cart|order:product_id={product_id};count={count};user_id={user_id}"
+)
