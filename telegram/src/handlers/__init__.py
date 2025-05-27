d4402ffaf98eaf8e05ed0695acf3a2aa176e6dc0
Владимир Волосников <volosnikovvladimir68@gmail.com>
2025-05-27 15:09:19 +0500

    Add import router from shopping_cart

--- telegram/src/handlers/__init__.py
+++ telegram/src/handlers/__init__.py
@@ -2,4 +2,5 @@
 
 from .catalog import router as catalog_router
 from .products import router as product_router
+from .shopping_cart import router as shopping_cart_router
 from .start_conversation import router as start_conversation_router
